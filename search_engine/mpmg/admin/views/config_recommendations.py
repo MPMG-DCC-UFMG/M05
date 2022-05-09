906f647f2bfe9d3add090a062fe1428dd5f22aa2
Elves Rodrigues <elvesmateusrodrigues@outlook.com>
2022-05-09 12:18:02 -0300

    Configuração do recomendador reabilitado

--- search_engine/mpmg/admin/views/config_recommendations.py
+++ search_engine/mpmg/admin/views/config_recommendations.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+import json
 from django.contrib import admin
 from django.shortcuts import render
 from mpmg.services.models import ConfigRecommendationSource, ConfigRecommendationEvidence
@@ -9,17 +11,19 @@ class ConfigRecommendationsView(admin.AdminSite):
     
 
     def view_config(self, request):
-        conf_rec_sources, _ = ConfigRecommendationSource().get()
-        conf_rec_evidences, _ = ConfigRecommendationEvidence().get()
+        conf_rec_sources = ConfigRecommendationSource().get()
+        conf_rec_evidences = ConfigRecommendationEvidence().get()
 
-        evidence_types = list(ev['evidence_type'] for ev in conf_rec_evidences)
+        evidence_types = list(ev['tipo_evidencia'] for ev in conf_rec_evidences)
 
-        conf_rec_sources.sort(key = lambda item: item['ui_name'])
-        conf_rec_evidences.sort(key = lambda item: item['ui_name'])
+        conf_rec_sources.sort(key = lambda item: item['nome'])
+        conf_rec_evidences.sort(key = lambda item: item['nome'])
 
         context = {
             'conf_rec_sources': conf_rec_sources,
+            'conf_rec_sources_json': json.dumps(conf_rec_sources),
             'conf_rec_evidences': conf_rec_evidences,
+            'conf_rec_evidences_json': json.dumps(conf_rec_evidences),
             'evidence_types': evidence_types
         }
         
--- search_engine/static/admin/js/recommendations.js
+++ search_engine/static/admin/js/recommendations.js
@@ -1,51 +1,61 @@
+function send_change_request(conf_type, data) {
+    $.ajax({
+        url: SERVICES_URL + `config_recommendation/${conf_type}`,
+        type: 'put',
+        dataType: 'json',
+        async: false,
+        data: data,
+        success: function (data) {
+        },
+        error: function (data) {
+            console.error(data);
+        }
+    });    
+}
+
 function update_config_rec_sources() {
     event.preventDefault();
+
     let confs = $('.rec_confs');
 
-    let config = {};
+    let config;
+    let data_changed;
     let config_type;
     
     let amount;
     let active;
 
+    let wait_for_changes = false;
+
     for (let i=0;i<confs.length;i++) {
         config_type = confs[i].getAttribute('config');
-        config[config_type] = {};
 
         amount = parseInt(confs[i].querySelector('.rec_size').value);
         active = confs[i].querySelector('.rec_active').checked;
+        
+        config = CONF_REC_SRC.filter(function(item) { return item.id == config_type;})[0];
 
-        if (amount != CONF_REC_SRC[config_type].amount)
-            config[config_type]['amount'] = amount;
+        data_changed = {};
 
-        if (active != CONF_REC_SRC[config_type].active) 
-            config[config_type].active = active;
+        if (amount != config.quantidade)
+            data_changed.quantidade = amount;
 
-        if (Object.keys(config[config_type]).length == 0)
-            delete config[config_type];
-    }
+        if (active != config.ativo)
+            data_changed.ativo = active;
 
-    if (Object.keys(config).length == 0) {
-        alert('Nenhuma alteração foi feita nos documentos a serem recomendados!');
-        return;
-    }
+        if (Object.keys(data_changed).length > 0) {
+            data_changed.id_conf_fonte = config_type;
+            wait_for_changes = true;
 
-    console.log(config);
-
-    $.ajax({
-        url: SERVICES_URL + 'config_recommendation/sources',
-        type: 'put',
-        dataType: 'json',
-        async: false,
-        // traditional: true,
-        data: config,
-        success: function (data) {
-            location.reload();
-        },
-        error: function () {
-            console.log('Não foi possível atualizar a configuração!');
+            send_change_request('sources', data_changed);
         }
-    });
+    }
+
+    if (wait_for_changes){
+        setTimeout(function () {
+            location.reload();
+        }, 750);
+    }
 
 }
 
@@ -54,7 +64,8 @@ function update_config_rec_evidences() {
 
     let confs = $('.ev_confs');
 
-    let config = {};
+    let config;
+    let data_changed;
     let config_type;
     
     let amount;
@@ -62,51 +73,47 @@ function update_config_rec_evidences() {
     let top_n_recommendations;
     let active;
 
+    let wait_for_changes = false;
+
     for (let i=0;i<confs.length;i++) {
         config_type = confs[i].getAttribute('config');
-        config[config_type] = {};
 
         amount = parseInt(confs[i].querySelector('.ev_amount').value);
         min_similarity = parseInt(confs[i].querySelector('.ev_min_sim').value);
         top_n_recommendations = parseInt(confs[i].querySelector('.ev_top_n').value);
         active = confs[i].querySelector('.ev_active').checked;
-
-        if (amount != CONF_REC_EV[config_type].amount)
-            config[config_type]['amount'] = amount;
         
-        if (min_similarity != CONF_REC_EV[config_type].min_similarity)
-            config[config_type]['min_similarity'] = min_similarity;
+        config = CONF_REC_EV.filter(function (item) { return item.id == config_type })[0];
+        data_changed = {};
 
-        if (top_n_recommendations != CONF_REC_EV[config_type].top_n_recommendations)
-            config[config_type]['top_n_recommendations'] = top_n_recommendations;
+        if (amount != config.quantidade)
+            data_changed.quantidade = amount;
+
+        if (min_similarity != config.similaridade_minima)
+            data_changed.similaridade_minima = min_similarity;
+
+        if (top_n_recommendations != config.top_n_recomendacoes)
+            data_changed.top_n_recomendacoes = top_n_recommendations;
+            
+        if (active != config.ativo)
+            data_changed.ativo = active;
+
         
-        if (active != CONF_REC_EV[config_type].active)
-            config[config_type]['active'] = active;
-        
-        if (Object.keys(config[config_type]).length == 0)
-            delete config[config_type];
+        if (Object.keys(data_changed).length > 0) {
+            data_changed.id_conf_evidencia = config_type;
+            wait_for_changes = true;
 
-    }
-
-    if (Object.keys(config).length == 0) {
-        alert('Nenhuma alteração foi feita nas fontes de recomendação!');
-        return;
-    }
-    
-    $.ajax({
-        url: SERVICES_URL + 'config_recommendation/evidences',
-        type: 'put',
-        dataType: 'json',
-        async: false,
-        // traditional: true,
-        data: config,
-        success: function (data) {
-            location.reload();
-        },
-        error: function () {
-            console.log('Não foi possível atualizar a configuração!');
+            send_change_request('evidences', data_changed);
         }
-    });
+    }
+
+    if (wait_for_changes) {
+        setTimeout(function () {
+            location.reload();
+        }, 950);
+    }
+
+   
 }
 
 function bind_label_to_range(id) {
@@ -115,51 +122,7 @@ function bind_label_to_range(id) {
     });
 }
 
-function get_confs() {
-    CONF_REC_SRC = {};
-    $.ajax({
-        url: SERVICES_URL + 'config_recommendation/sources',
-        type: 'get',
-        dataType: 'json',
-        // headers: { 'Authorization': 'Token ' + AUTH_TOKEN },
-        success: function (data) {
-            for (let i=0;i<data.length;i++) {
-                CONF_REC_SRC[data[i].es_index_name] = {
-                    active: data[i].active,
-                    amount: data[i].amount,
-                }
-            }
-        },
-        error: function () {
-            console.log('Não foi possível obter config. sources!');
-        }
-    });
-
-    CONF_REC_EV = {};
-    $.ajax({
-        url: SERVICES_URL + 'config_recommendation/evidences',
-        type: 'get',
-        dataType: 'json',
-        // headers: { 'Authorization': 'Token ' + AUTH_TOKEN },
-        success: function (data) {
-            for (let i=0;i<data.length;i++) {
-                CONF_REC_EV[data[i].evidence_type] = {
-                    active: data[i].active,
-                    amount: data[i].amount,
-                    min_similarity: data[i].min_similarity,
-                    top_n_recommendations: data[i].top_n_recommendations
-                }
-            }
-        },
-        error: function () {
-            console.log('Não foi possível obter config. evidences!');
-        }
-    });
-}
-
 $(document).ready(function() {
     for (let i=0;i<EVIDENCE_TYPES.length;i++)
         bind_label_to_range(`ev_${EVIDENCE_TYPES[i]}_min_sim`);
-    
-    get_confs();
 });
