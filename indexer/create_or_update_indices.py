677161f53007fe43fb09efcfca85dd3dfa402ad1
Rennan Cordeiro <rennancl@gmail.com>
2022-05-18 11:17:07 -0300

    updates create indices

--- indexer/create_or_update_indices.py
+++ indexer/create_or_update_indices.py
@@ -36,9 +36,9 @@ def same_keys_values(local_dict, elastic_dict):
 def main(maps_sets_path, default_data_path, elastic_address, elastic_username=None, elastic_password=None, force_creation=[]):
 
     if elastic_username != None and elastic_password != None:
-        es = Elasticsearch([elastic_address], http_auth=(elastic_username, elastic_password))
+        es = Elasticsearch([elastic_address], timeout=120, max_retries=3, retry_on_timeout=True, http_auth=(elastic_username, elastic_password))
     else:
-        es = Elasticsearch([elastic_address])
+        es = Elasticsearch([elastic_address], timeout=120, max_retries=3, retry_on_timeout=True)
 
     # pega o mappings e o settings de cada índice no diretório e verifica se ele existe no elastic
     # se existir no elastic, pega o mappings e o settings, pra comparação futura
@@ -48,7 +48,7 @@ def main(maps_sets_path, default_data_path, elastic_address, elastic_username=No
         index_name = filename.split('.')[0]
         index_map_set = json.load(open(os.path.join(maps_sets_path, filename)))
         directory_indices[index_name] = index_map_set
-        
+
         if es.indices.exists(index=index_name):
             elastic_map = es.indices.get_mapping(index=index_name)
             elastic_set = es.indices.get_settings(index=index_name)
@@ -56,7 +56,7 @@ def main(maps_sets_path, default_data_path, elastic_address, elastic_username=No
                 'mappings': elastic_map[index_name]['mappings'],
                 'settings': elastic_set[index_name]['settings']
             }
-    
+
     # carrega os dados default para serem inseridos após a criação dos índices
     # (Como são poucos dados, carrego tudo na memória mesmo.)
     default_indices_data = {}
@@ -65,25 +65,25 @@ def main(maps_sets_path, default_data_path, elastic_address, elastic_username=No
         index_data = json.load(open(os.path.join(default_data_path, filename)))
         default_indices_data[index_name] = index_data
 
-    
+
     c = 0
     for index_name, index_map_set in directory_indices.items():
         just_created = False
 
         # deleta o índice caso esteja na lista de force_creation e de fato exista no elastic
         if index_name in elastic_indices and ('all' in force_creation or index_name in force_creation):
-            es.indices.delete(index_name)
+            es.indices.delete(index=index_name)
             del elastic_indices[index_name]
             print('Índice removido forçadamente:', index_name)
             c += 1
-        
+
         # deleta o índice caso ele seja diferente do mappings do directory
         if index_name in elastic_indices and not same_keys_values(directory_indices[index_name]['mappings'], elastic_indices[index_name]['mappings']):
             es.indices.delete(index=index_name)
             del elastic_indices[index_name]
             print('Índice removido por estar diferente:', index_name)
             c += 1
-        
+
         # cria o índice caso não exista no elastic
         if index_name not in elastic_indices:
             just_created = True
@@ -96,11 +96,11 @@ def main(maps_sets_path, default_data_path, elastic_address, elastic_username=No
             if index_name in default_indices_data:
                 num_data = 0
                 for item in default_indices_data[index_name]['data']:
-                    es.index(index=index_name, id=item['id'], body=item['body'])
+                    es.index(index=index_name, id=item['id'], document=item['body'])
                     num_data += 1
                 print('Registros default inseridos no índice '+index_name+':', num_data)
 
-        
+
         # atualiza o settings caso esteja definido no directory e exista diferença com o elastic
         # se o índice acabou de ser criado, esse passo pode ser pulado (pq não haverá diferença lol)
         if just_created == False and 'settings' in directory_indices[index_name]:
@@ -109,19 +109,16 @@ def main(maps_sets_path, default_data_path, elastic_address, elastic_username=No
                 es.indices.put_settings(index = index_name, body = directory_indices[index_name]['settings'])
                 print('Settings atualizado:', index_name)
                 c += 1
-        
 
     if c == 0:
         print('Nenhum índice foi criado ou atualizado.')
-    
-
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Cria os índices de acordo com o arquivo de mappings. Irá criar apenas os \
     índices que não existem. Passe -force_creation para recriar mesmo os índices já existentes (isto irá apagar todos os dados).')
-    
+
     parser.add_argument("-maps_sets_path", default="indices_mappings_settings", help="Caminho do diretório com os mappings e settings de cada índice")
     parser.add_argument("-default_data_path", default="indices_default_data", help="Caminho do diretório com os dados padrões dos índices")
     parser.add_argument("-elastic_address", default="localhost:9200", help="Endereço do elasticsearch no formato: <ip>:<port>")
