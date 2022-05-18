1a3815c737a13f9ccd19bb6a0ba82890e62a3f9e
Elves Rodrigues <elvesmateusrodrigues@outlook.com>
2022-05-17 21:16:03 -0300

    Interface parcial para rec. de entidades

---
+++ search_engine/mpmg/services/models/config_rec_entities.py
@@ -0,0 +1,18 @@
+from mpmg.services.models.elastic_model import ElasticModel
+
+class ConfigRecEntity(ElasticModel):
+    index_name = 'config_rec_entities'
+
+    def __init__(self, **kwargs):
+        index_name = ConfigRecEntity.index_name
+        meta_fields = ['id', 'description', 'type']
+        index_fields = [
+            'icone',
+            'nome',
+            'tipo_entidade',
+            'tecnica_agregacao',
+            'num_itens_recomendados'
+        ]
+
+        super().__init__(index_name, meta_fields, index_fields, **kwargs)
+     
