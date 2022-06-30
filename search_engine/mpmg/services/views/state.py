c9bc4bec2c19b185b2171c4ba5948503c210f0e9
Elves Rodrigues <elvesmateusrodrigues@outlook.com>
2022-06-30 09:19:19 -0300

    API para estados

--- search_engine/mpmg/services/views/state.py
+++ search_engine/mpmg/services/views/state.py
@@ -13,8 +13,8 @@ class StateView(APIView):
     schema = AutoDocstringSchema()
 
     def get(self, request):
-        if 'id' in request.GET:
-            state = STATE.get(request.GET['id'])
+        if 'id_estado' in request.GET:
+            state = STATE.get(request.GET['id_estado'])
 
             if state is None:
                 return Response({'message': 'O estado não existe ou não foi encontrado.'}, status=status.HTTP_404_NOT_FOUND)
@@ -25,10 +25,75 @@ class StateView(APIView):
         return Response(states, status=status.HTTP_200_OK)            
 
     def post(self, request):
-        pass 
+        data = get_data_from_request(request)
+        
+        expected_fields = {'codigo', 'sigla', 'nome'}    
+
+        all_fields_available, unexpected_fields_message = validators.all_expected_fields_are_available(data, expected_fields)
+        if not all_fields_available:
+            return Response({'message': unexpected_fields_message}, status=status.HTTP_400_BAD_REQUEST)
+
+        STATE.parse_data_type(data)
+
+        state_id = STATE.save(dict(
+            codigo = data['codigo'],
+            sigla = data['sigla'],
+            nome = data['nome']
+        ))        
+
+        if state_id:
+            return Response({'id_estado': state_id}, status=status.HTTP_200_OK)
+
+        return Response({'message': 'Não foi possível criar a estado, tente novamente!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+   
 
     def put(self, request):
-        pass 
+        data = get_data_from_request(request)
+        
+        state_id = data.get('id_estado')
+        if state_id is None:
+            return Response({'message': 'Informe o ID do estado a ser editado.'}, status.HTTP_400_BAD_REQUEST)
+        del data['id_estado']
+
+        state = STATE.get(state_id)
+
+        if state is None:
+            return Response({'message': 'O estado não existe ou não foi encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+
+        valid_fields = {'sigla', 'codigo', 'nome'} 
+        data_fields_valid, unexpected_fields_message = validators.some_expected_fields_are_available(data, valid_fields)
+
+        if not data_fields_valid:
+            return Response({'message': unexpected_fields_message}, status=status.HTTP_400_BAD_REQUEST)
+
+        STATE.parse_data_type(data)
+
+        if STATE.item_already_updated(state, data):
+            return Response({'message': 'O estado já está atualizado.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+                
+        success = STATE.update(state_id, data)
+        if success:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response({'message': 'Não foi possível atualizar o estado, tente novamente.'}, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
     def delete(self, request):
-        pass 
+        data = get_data_from_request(request)
+
+        try:
+            state_id = data['id_estado']
+        
+        except KeyError:
+            return Response({'message': 'Informe o ID do a ser deletado.'}, status.HTTP_400_BAD_REQUEST)
+
+        state = STATE.get(state_id)
+        if state is None:
+           return Response({'message': 'O estado não existe ou não foi encontradao.'}, status=status.HTTP_404_NOT_FOUND)
+
+        success = STATE.delete(state_id)
+        if success:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response({'message': 'Não foi possível remover o estado, tente novamente.'}, status.HTTP_500_INTERNAL_SERVER_ERROR)
