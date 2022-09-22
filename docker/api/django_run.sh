3049cf3a1554f4ebd6bfdd4ba2cb59358fa6c1c1
Rennan Cordeiro <rennancl@gmail.com>
2022-09-22 13:24:29 -0300

    updaes docker related files

--- docker/api/django_run.sh
+++ docker/api/django_run.sh
@@ -2,11 +2,20 @@
 
 python3 manage.py makemigrations --check --noinput
 python3 manage.py migrate --noinput
-if [ "$DJANGO_SUPERUSER_USERNAME" ]
+export DJANGO_SUPERUSER_PASSWORD=$DJANGO_GSI_SUPERUSER_PASSWORD;
+if [ "$DJANGO_GSI_SUPERUSER_USERNAME" ]
 then
     python manage.py createsuperuser \
         --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --api_client_name $DJANGO_SUPERUSER_CLIENT
+        --username $DJANGO_GSI_SUPERUSER_USERNAME \
+        --api_client_name $DJANGO_GSI_SUPERUSER_CLIENT
+fi
+export DJANGO_SUPERUSER_PASSWORD=$DJANGO_PROCON_SUPERUSER_PASSWORD;
+if [ "$DJANGO_PROCON_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_PROCON_SUPERUSER_USERNAME \
+        --api_client_name $DJANGO_PROCON_SUPERUSER_CLIENT
 fi
 python3 manage.py runserver 0.0.0.0:8087
